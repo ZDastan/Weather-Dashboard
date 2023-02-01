5087472c4bbc07c7cb82a91fc22cf69035ce833d
zehra <zehradastan@hotmail.com>
2023-02-01 13:07:55 +0000

    api function updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var searchOptions = document.querySelector('#city-search');
-var searchButtonsEl = document.querySelector('#search-buttons');
+//var searchButtonsEl = document.querySelector('#search-buttons');
 var cityInputName = document.querySelector('#cityname');
 var forecastEl = document.querySelector('#forecast');
 var fiveDayEl = document.querySelector('#fiveday');
@@ -7,10 +7,11 @@ var fiveDayEl = document.querySelector('#fiveday');
 //If city name not type and click search button
 var formSubmitHandler = function (event) {
     event.preventDefault();
-    
-    if (cityInputName) {
-        (cityInputName);
-        forecastEl.textContent ='';
+    var city = cityInputName.value.trim();
+    if (city) {
+        // (cityInputName);
+        // forecastEl.textContent ='';
+        getApi(city)
         cityInputName.value='';
 
     }
@@ -19,21 +20,25 @@ var formSubmitHandler = function (event) {
 
 //Seach button click event
 var buttonClickHandler = function (event) {
+    event.preventDefault();
     var city = event.target.getAttribute('data-city');
-    if(cityInputName){
-        (cityInputName);
-        forecastEl.textContent ='';
-
+    
+    if(city){
+        console.log(city);
+        //(cityInputName);
+        //forecastEl.textContent ='';
+        
     }
+    
 };
 
 //Call API
 var APIKey="89bf41583e30d5bf31930db45b5bf434";
-//var city = ["Atlanta", "Denver", "Seattle", "San Francisco", "Orlando", "New York", "Chicago", "Austin"];
-var city = cityInputName.value.trim();
 
 
-function getApi() {
+
+
+function getApi(city) {
     var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid='+APIKey;
     fetch(requestUrl)
     .then(function (response) {
@@ -44,7 +49,7 @@ function getApi() {
     });
 }
 
-getApi()
+
 
 //curent weather
 // var nameValue = data['name'];
@@ -60,4 +65,4 @@ getApi()
 
 //Event listeners
 searchOptions.addEventListener('submit', formSubmitHandler);
-searchButtonsEl.addEventListener('click', buttonClickHandler);
+//searchButtonsEl.addEventListener('click', buttonClickHandler);
