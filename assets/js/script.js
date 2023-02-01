68662bf201ef1febdafa4d285e9588a5f31cb011
zehra <zehradastan@hotmail.com>
2023-02-01 10:12:59 +0000

    buttons cleared

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var searchOptions = document.querySelector('#city-search');
-var cityButtonsEl = document.querySelector('#city-buttons');
+var searchButtonsEl = document.querySelector('#search-buttons');
 var cityInputName = document.querySelector('#cityname');
 var forecastEl = document.querySelector('#forecast');
 var fiveDayEl = document.querySelector('#fiveday');
@@ -7,13 +7,12 @@ var fiveDayEl = document.querySelector('#fiveday');
 //If city name not type and click search button
 var formSubmitHandler = function (event) {
     event.preventDefault();
-    if (cityname) {
-        getCityName(cityname);
+    var username = nameInputEl.value.trim();
+    if (cityInputName) {
+        (cityInputName);
         forecastEl.textContent ='';
         cityInputName.value='';
 
-    }else{
-        alert('Please enter city name');
     }
 };
 
@@ -21,8 +20,8 @@ var formSubmitHandler = function (event) {
 //Seach button click event
 var buttonClickHandler = function (event) {
     var city = event.target.getAttribute('data-city');
-    if(city){
-        getFeatureForecast(city);
+    if(cityInputName){
+        (cityInputName);
         forecastEl.textContent ='';
 
     }
@@ -30,10 +29,12 @@ var buttonClickHandler = function (event) {
 
 //Call API
 var APIKey="89bf41583e30d5bf31930db45b5bf434";
-var city = ["Atlanta", "Denver", "Seattle", "San Francisco", "Orlando", "New York", "Chicago", "Austin"];
+//var city = ["Atlanta", "Denver", "Seattle", "San Francisco", "Orlando", "New York", "Chicago", "Austin"];
+var city = cityInputName.value.trim();
+
 
 function getApi() {
-    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q ='city'+ &appid= +'APIKey'";
+    var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid='+APIKey;
     fetch(requestUrl)
     .then(function (response) {
       return response.json();
@@ -43,4 +44,20 @@ function getApi() {
     });
 }
 
+getApi()
 
+//curent weather
+// var nameValue = data['name'];
+// var temperature = data['main']['temp'];
+// var wind =data['wind'];
+// var humiditiy = data['main']['humidity'];
+// var descValue = data
+
+
+
+
+
+
+//Event listeners
+searchOptions.addEventListener('submit', formSubmitHandler);
+searchButtonsEl.addEventListener('click', buttonClickHandler);
